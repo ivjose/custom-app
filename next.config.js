360ccd7f95b466bea03ef59a1c3c6c0cc7959de6
“Jose <ivjosesantos@gmail.com>
2023-01-06 21:29:27 +0800

    New update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
